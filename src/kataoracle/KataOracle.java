8ba3921ca3b8565638a71b5a312f100548ebc33f
usuario <usuario@10.22.146.199>
2013-11-29 12:19:53 +0000

    Primer commit

---
+++ src/kataoracle/KataOracle.java
@@ -0,0 +1,15 @@
+package kataoracle;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import oracle.jdbc.OracleDriver;
+
+public class KataOracle {
+
+    public static void main(String[] args) throws SQLException {
+        DriverManager.registerDriver(new OracleDriver());
+        Connection connection = DriverManager.getConnection("jdbc:orcl:localhost:100");
+        
+    }
+}
